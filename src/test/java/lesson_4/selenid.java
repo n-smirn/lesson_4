d90679fbb54ac4fd3f73ef37af9718a6d169a55f
n-smirn <smirnovaswan@gmail.com>
2023-07-28 01:57:49 +0500

    итоговый

--- src/test/java/lesson_4/selenid.java
+++ src/test/java/lesson_4/selenid.java
@@ -1,9 +1,12 @@
 package lesson_4;
 
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -15,14 +18,27 @@ public class Selenid {
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void fillRegFormTest() {
-        open("/selenide/selenide");
+    void selenidWikiTest() {
+        String JUnit5CodeExample = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
+
+                open("/selenide/selenide");
         $("#wiki-tab").click();
-        //Перейдите в раздел Wiki проекта
-        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").$(byText("3. Using JUnit5 extend test class:")).sibling(0).shouldHave(text(JUnit5CodeExample));
     }}
